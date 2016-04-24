04176247fb536c433fe84e4c3e9bf3a7276c309b
Christoph Fabianek <christoph.fabianek@gmail.com>
2016-04-25 00:09:25 +0200

    added target dir

---
+++ target/generated-sources/eu/ownyourdata/pia/web/rest/mapper/PluginMapperImpl.java
@@ -0,0 +1,12 @@
+package eu.ownyourdata.pia.web.rest.mapper;
+
+import javax.annotation.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    comments = "version: 1.0.0.Final, compiler: javac, environment: Java 1.8.0_73 (Oracle Corporation)"
+)
+@Component
+public class PluginMapperImpl extends PluginMapper {
+}
---
+++ target/pia-0.6.1-SNAPSHOT/scripts/app/entities/plugin/plugin.js
@@ -0,0 +1,122 @@
+'use strict';
+
+angular.module('piaApp')
+    .config(function ($stateProvider) {
+        $stateProvider
+            .state('plugin', {
+                parent: 'entity',
+                url: '/plugins',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'piaApp.plugin.home.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/plugin/plugins.html',
+                        controller: 'PluginController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('plugin');
+                        $translatePartialLoader.addPart('global');
+                        return $translate.refresh();
+                    }]
+                }
+            })
+            .state('plugin.detail', {
+                parent: 'entity',
+                url: '/plugin/{id}',
+                data: {
+                    authorities: ['ROLE_USER'],
+                    pageTitle: 'piaApp.plugin.detail.title'
+                },
+                views: {
+                    'content@': {
+                        templateUrl: 'scripts/app/entities/plugin/plugin-detail.html',
+                        controller: 'PluginDetailController'
+                    }
+                },
+                resolve: {
+                    translatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate, $translatePartialLoader) {
+                        $translatePartialLoader.addPart('plugin');
+                        return $translate.refresh();
+                    }],
+                    entity: ['$stateParams', 'Plugin', function($stateParams, Plugin) {
+                        return Plugin.get({id : $stateParams.id});
+                    }]
+                }
+            })
+            .state('plugin.edit', {
+                parent: 'plugin',
+                url: '/{id}/edit',
+                data: {
+                    authorities: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/plugin/plugin-dialog.html',
+                        controller: 'PluginDialogController',
+                        size: 'lg',
+                        resolve: {
+                            entity: ['Plugin', function(Plugin) {
+                                return Plugin.get({id : $stateParams.id});
+                            }]
+                        }
+                    }).result.then(function(result) {
+                        $state.go('plugin', null, { reload: true });
+                    }, function() {
+                        $state.go('^');
+                    })
+                }]
+            })
+            .state('plugin.delete', {
+                parent: 'plugin',
+                url: '/{id}/delete',
+                data: {
+                    authorities: ['ROLE_USER'],
+                },
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/plugin/plugin-delete-dialog.html',
+                        controller: 'PluginDeleteController',
+                        size: 'md',
+                        resolve: {
+                            entity: ['Plugin', function(Plugin) {
+                                return Plugin.get({id : $stateParams.id});
+                            }]
+                        }
+                    }).result.then(function(result) {
+                        $state.go('plugin', null, { reload: true });
+                    }, function() {
+                        $state.go('^');
+                    })
+                }]
+            })
+            .state('plugin.register', {
+                onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                    $uibModal.open({
+                        templateUrl: 'scripts/app/entities/plugin/plugin-register-dialog.html',
+                        controller: 'PluginRegisterDialogController',
+                        size: 'lg',
+                    }).result.then(function(result) {
+                        $state.go('plugin', null, { reload: true });
+                    }, function() {
+                        $state.go('plugin', null, { reload: true });
+                    })
+                }]
+            })
+        .state('plugin.install', {
+            onEnter: ['$stateParams', '$state', '$uibModal', function($stateParams, $state, $uibModal) {
+                $uibModal.open({
+                    templateUrl: 'scripts/app/entities/plugin/plugin-install-dialog.html',
+                    controller: 'PluginInstallDialogController',
+                    size: 'lg',
+                }).result.then(function(result) {
+                    $state.go('plugin', null, { reload: true });
+                }, function() {
+                    $state.go('plugin', null, { reload: true });
+                })
+            }]
+        });
+    });
